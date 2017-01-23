f54facc77cd5ded481b738b315a99bc6f339f0ea
kseyawingit <callmeloya@mail.ru>
2017-01-23 22:33:45 +0300

    Addet toolbar and search button.

--- app/src/main/java/com/netsmake/reminder/MainActivity.java
+++ app/src/main/java/com/netsmake/reminder/MainActivity.java
@@ -1,5 +1,6 @@
 package com.netsmake.reminder;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -7,15 +8,24 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int LAYOUT = R.layout.activity_main;
+
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
 
 
+        initToolbar();
+        initNavigationView();
+
+    }
+
+    private void initToolbar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -25,6 +35,11 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    private void initNavigationView() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
 
     }
 
