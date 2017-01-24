146658a9a3d90180e5f3852aa11b04f579142a70
kseyawingit <callmeloya@mail.ru>
2017-01-24 14:38:56 +0300

    implemented navigation view and tab navigation with test fragment.

--- app/src/main/java/com/netsmake/reminder/MainActivity.java
+++ app/src/main/java/com/netsmake/reminder/MainActivity.java
@@ -1,17 +1,25 @@
 package com.netsmake.reminder;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.netsmake.reminder.adapter.TabsPagerFragmentAdapter;
+
 public class MainActivity extends AppCompatActivity {
     private static final int LAYOUT = R.layout.activity_main;
 
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
         initToolbar();
         initNavigationView();
+        initTabs();
 
     }
 
@@ -37,10 +46,36 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void initTabs() {
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+       //tabLayout.addTab();
+        TabsPagerFragmentAdapter adapter = new TabsPagerFragmentAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+
     private void initNavigationView() {
+
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.view_navigation_open, R.string.view_navigation_close);
+        //drawerLayout.setDrawerListener(toggle);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
-
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                drawerLayout.closeDrawers();
+                switch (item.getItemId()) {
+                    case R.id.actionNotificationItem:
+                        showNotificationTab();
+                }
+                return true;
+            }
+        });
     }
 
     @Override
@@ -50,5 +85,8 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    private void showNotificationTab () {
+        viewPager.setCurrentItem(Constants.TAB_TWO);
+    }
 
 }
